2560dab1443a9670bb501658db0ed789547bc100
zhud2xx <zhud2xx@cse121596.uta.edu>
2018-10-26 10:49:47 -0500

    aaa

--- djDICCCOLService/src/edu/uga/DICCCOL/stat/Correlation.java
+++ djDICCCOLService/src/edu/uga/DICCCOL/stat/Correlation.java
@@ -2,15 +2,16 @@ package edu.uga.DICCCOL.stat;
 
 import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 
-
 public class Correlation {
-	
-	public double Correlation_Pearsons(double[] dataSet1, double[] dataSet2)
-	{
-		double correlation_p=-10.0d;
+
+	public double Correlation_Pearsons(double[] dataSet1, double[] dataSet2) {
+		double correlation_p = -10.0d;
 		PearsonsCorrelation correlationHandler = new PearsonsCorrelation();
 		correlation_p = correlationHandler.correlation(dataSet1, dataSet2);
-		return correlation_p;
+		if (Double.isNaN(correlation_p))
+			return 0.0;
+		else
+			return correlation_p;
 	}
 
 }
