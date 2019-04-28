c0ddaed1cb81209bb84b15b1b6c283002373a370
zhud2xx <zhud2xx@192.168.1.6>
2019-04-27 21:29:52 -0500

    asdf

--- djDICCCOLService/src/edu/uga/DICCCOL/DicccolUtil.java
+++ djDICCCOLService/src/edu/uga/DICCCOL/DicccolUtil.java
@@ -1,8 +1,11 @@
 package edu.uga.DICCCOL;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+
+import edu.uga.liulab.djVtkBase.djVtkSurData;
 
 public class DicccolUtil {
 	
@@ -80,5 +83,50 @@ public class DicccolUtil {
 			result += String.valueOf(data[i])+" "; 
 		return result;
 	}
+	
+    public static void surfaceCombine(List<String> surFileList, String outPutFile)
+    {
+    	System.out.println("****************** There are "+surFileList.size()+" surface files need to be combined! ******************");
+    	List<djVtkSurData> surDataList = new ArrayList<djVtkSurData>();
+    	for(int i=0;i<surFileList.size();i++)
+    		surDataList.add( new djVtkSurData(surFileList.get(i).trim()) );
+    	
+    	int startPtIndex = 0;
+    	int startCellIndex = 0;
+    	surDataList.get(0).cellsOutput.addAll(surDataList.get(0).getAllCells());
+    	for(int i=1;i<surFileList.size();i++)
+    	{
+    		System.out.println("Combining "+surFileList.get(i)+"...");
+    		startPtIndex = surDataList.get(0).points.size();
+    		for(int p=0;p<surDataList.get(i).nPointNum;p++)
+    		{
+    			surDataList.get(i).getPoint(p).pointId = startPtIndex+p;
+    			surDataList.get(0).points.add( surDataList.get(i).getPoint(p) );
+    		}
+    		
+    		startCellIndex = surDataList.get(0).cells.size();
+    		for(int c=0;c<surDataList.get(i).nCellNum;c++)
+    		{
+    			surDataList.get(i).getcell(c).cellId = startCellIndex+c;
+    			surDataList.get(0).cells.add( surDataList.get(i).getcell(c) );
+    		}
+    		
+    		surDataList.get(0).nPointNum = surDataList.get(0).points.size();
+    		surDataList.get(0).nCellNum = surDataList.get(0).cells.size();
+    		
+    		surDataList.get(0).cellsOutput.addAll(surDataList.get(i).getAllCells());
+    		if (surDataList.get(0).pointsScalarData.size() > 0) {
+    			Iterator iterCellData = surDataList.get(0).pointsScalarData.keySet().iterator();
+    			while (iterCellData.hasNext()) {
+    				String tmpAttriName = (String) iterCellData.next();
+    				List<String> newAttriList = surDataList.get(0).pointsScalarData.get(tmpAttriName);
+    				newAttriList.addAll(surDataList.get(i).pointsScalarData.get(tmpAttriName));
+    				surDataList.get(0).pointsScalarData.remove(tmpAttriName);
+    				surDataList.get(0).pointsScalarData.put(tmpAttriName, newAttriList);
+    				}
+    			} //if
+    	} //for i=1
+    	surDataList.get(0).writeToVtkFile(outPutFile);
+    }
 
 }
